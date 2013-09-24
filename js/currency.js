58c817110cf7b236f3bb2aedb0c72784f2905901
Ajith K M <km.ajith.km@gmail.com>
2013-09-25 03:51:47 +0530

    completed upto autocomplete

--- js/currency.js
+++ js/currency.js
@@ -1,3 +1,8 @@
+
+			   
+			
+		 
+
 			function getcurrency(){
 				$('#quot').html('');
 				document.getElementById('loading').show();
@@ -15,19 +20,4 @@
 						$('#quot').html('Conversion rate is : ' + data.rate);
 						});
 					}
-			};
-			function getstock(){
-				$('#quotstocknse').html('');
-				stock = document.getElementById('stocks').value;
-				document.getElementById('loading2').show();
-				var response = new String;
-				$.get("http://www.google.com/finance/info?q=NSE:"+stock,function(data,status){
-					response=data;
-					var values = response.split(",");
-					var l = values[3].split(":");
-					var last = l[1].substring(2,l[1].lastIndexOf("\""));
-					document.getElementById('loading2').hide();
-					$('#quotstockbse').html('Last Traded Value is : ' + last );
-				});
-			};
-			
+			}
